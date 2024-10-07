d32f1d03d66b144ff7c4177fdad5ac9ad49587d4
Philip Xie Zhen <huakeng@csu.fullerton.edu>
2024-10-07 15:58:39 -0700

    fixed output error from submission 1

--- patterns.c
+++ patterns.c
@@ -8,61 +8,64 @@
 #include <unistd.h>
 
 void pattern1(int processes) {
-    printf("** Pattern 1: creating %d processes **\n", processes);
+    fprintf(stderr, "** Pattern 1: creating %d processes **\n", processes);
     for (int ix = 1; ix <= processes; ix++) {
         pid_t child_pid = fork();
         if (child_pid == 0) {  // Child Process
-            printf("Process %d (%d) beginning\n", ix, getpid());
+            fprintf(stderr, "Process %d (%d) beginning\n", ix, getpid());
             int sleep_time = rand() % 8 + 1;
-            printf("Process %d (pid %d): sleeping for %d seconds\n", ix,
-                   getpid(), sleep_time);
+            fprintf(stderr, "Process %d (pid %d): sleeping for %d seconds\n",
+                    ix, getpid(), sleep_time);
             sleep(sleep_time);
-            printf("Process %d (pid %d): exiting.\n", ix, getpid());
+            fprintf(stderr, "Process %d (pid %d): exiting.\n", ix, getpid());
             exit(0);
         } else {  // Parent Process
-            printf("Parent: created child %d (pid %d)\n", ix, child_pid);
+            fprintf(stderr, "Parent: created child %d (pid %d)\n", ix,
+                    child_pid);
         }
     }
     // Wait for all child processes to complete
     while (wait(NULL) > 0);
-    printf("** Pattern 1: All children have exited **\n");
+    fprintf(stderr, "** Pattern 1: All children have exited **\n");
 }
 
 void pattern2(int processes) {
-    printf("** Pattern 2: creating %d processes **\n", processes);
+    fprintf(stderr, "** Pattern 2: creating %d processes **\n", processes);
     for (int ix = 1; ix <= processes; ix++) {
         pid_t pid = getpid();
         int sleep_time = rand() % 8 + 1;
 
         if (ix == 0) {
-            printf("Parent: created child %d (pid %d)\n", ix, pid);
+            fprintf(stderr, "Parent: created child %d (pid %d)\n", ix, pid);
         } else {
-            printf("Child %d (pid %d): starting\n", ix, pid);
+            fprintf(stderr, "Child %d (pid %d): starting\n", ix, pid);
         }
-        
+
         // Create next process in this current process
         if (ix < processes) {
             pid_t child_pid = fork();
             if (child_pid == 0) {  // Child Process
-                printf(
-                    "Child %d (pid %d), sleeping %d seconds after creating "
-                    "child %d (pid %d)\n",
-                    ix, getpid(), sleep_time, ix + 1, getpid());
+                fprintf(stderr,
+                        "Child %d (pid %d), sleeping %d seconds after creating "
+                        "child %d (pid %d)\n",
+                        ix, getpid(), sleep_time, ix + 1, getpid());
                 sleep(sleep_time);
             } else {  // Parent Process
-                printf("Child %d (pid %d), waiting for child %d (pid %d)\n", ix,
-                       pid, ix + 1, child_pid);
+                fprintf(stderr,
+                        "Child %d (pid %d), waiting for child %d (pid %d)\n",
+                        ix, pid, ix + 1, child_pid);
                 wait(NULL);
-                printf("Child %d (pid %d) exiting.\n", ix, pid);
+                fprintf(stderr, "Child %d (pid %d) exiting.\n", ix, pid);
                 exit(0);
             }
         } else {  // Special case for the last process since it doesn't fork a
                   // child
-            printf(
+            fprintf(
+                stderr,
                 "Child %d (pid %d) [no children created] sleeping %d seconds\n",
                 ix, pid, sleep_time);
             sleep(sleep_time);  // Sleep for a random duration
-            printf("Child %d (pid %d) exiting.\n", ix, pid);
+            fprintf(stderr, "Child %d (pid %d) exiting.\n", ix, pid);
         }
     }
 }
